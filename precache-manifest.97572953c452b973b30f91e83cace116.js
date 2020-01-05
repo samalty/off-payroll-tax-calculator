9501b9c2c4fb645a4a98d803260439c5b83a647d
Sam Alty <sam_alty@outlook.com>
2020-01-05 16:40:49 +0000

    Updates

---
+++ precache-manifest.97572953c452b973b30f91e83cace116.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3dd162ca8acd98c4c51acb4dfdcd3d7",
+    "url": "/off-payroll-tax-calculator/index.html"
+  },
+  {
+    "revision": "ac4cc9165e850ca0209e",
+    "url": "/off-payroll-tax-calculator/static/css/main.63ade890.chunk.css"
+  },
+  {
+    "revision": "b182126a0cfe4800517a",
+    "url": "/off-payroll-tax-calculator/static/js/2.ecac501a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/off-payroll-tax-calculator/static/js/2.ecac501a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac4cc9165e850ca0209e",
+    "url": "/off-payroll-tax-calculator/static/js/main.c1469daf.chunk.js"
+  },
+  {
+    "revision": "020de24c76b66c66dd25",
+    "url": "/off-payroll-tax-calculator/static/js/runtime-main.194af345.js"
+  },
+  {
+    "revision": "6ca515b52b9c5cfd8f3d4b41ba5fc893",
+    "url": "/off-payroll-tax-calculator/static/media/off-payroll.6ca515b5.png"
+  }
+]);
